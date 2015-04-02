6d2a270df94aed76065069fa9b4f404381ba102d
Jeff Smith <jffreyjs@gmail.com>
2015-04-01 22:39:04 -0400

    refactor specs, load opcodes

--- spec/opcodes/loadsSpec.js
+++ spec/opcodes/loadsSpec.js
@@ -4,7 +4,7 @@ var loads = require('../../lib/opcodes/loads');
 
 describe("Load opcodes", function() {
   var cpu, mmu, ops;
-  
+
   beforeEach(function() {
     cpu = new CPU();
     mmu = new MMU();
@@ -16,156 +16,115 @@ describe("Load opcodes", function() {
     beforeEach(function() {
       spyOn(mmu, 'read8').and.returnValue(0x1A);
     });
-    
-    describe("LD B, n", function() {
-      it("loads immediate 8-bit value into register B", function() {
-        ops[0x06]();
-        expect(cpu.registers.B).toEqual(0x1A);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x201);
-      });
-    });
 
-    describe("LD C, n", function() {
-      it("loads immediate 8-bit value into register C", function() {
-        ops[0x0E]();
-        expect(cpu.registers.C).toEqual(0x1A);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x201);
-      });
-    });
-
-    describe("LD D, n", function() {
-      it("loads immediate 8-bit value into register D", function() {
-        ops[0x16]();
-        expect(cpu.registers.D).toEqual(0x1A);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x201);
-      });
-    });
-    
-    describe("LD E, n", function() {
-      it("loads immediate 8-bit value into register D", function() {
-        ops[0x1E]();
-        expect(cpu.registers.E).toEqual(0x1A);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x201);
-      });
-    });
-
-    describe("LD H, n", function() {
-      it("loads immediate 8-bit value into register D", function() {
-        ops[0x26]();
-        expect(cpu.registers.H).toEqual(0x1A);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x201);
-      });
-    });
-
-    describe("LD L, n", function() {
-      it("loads immediate 8-bit value into register D", function() {
-        ops[0x2E]();
-        expect(cpu.registers.L).toEqual(0x1A);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x201);
+    [
+      { dest: 'A', op: 0x3E },
+      { dest: 'B', op: 0x06 },
+      { dest: 'C', op: 0x0E },
+      { dest: 'D', op: 0x16 },
+      { dest: 'E', op: 0x1E },
+      { dest: 'H', op: 0x26 },
+      { dest: 'L', op: 0x2E }
+    ].forEach(function(i) {
+      describe("LD " + i.dest + ",n", function() {
+        it("loads immediate 8-bit value into register " + i.dest, function() {
+          ops[i.op]();
+          expect(cpu.registers[i.dest]).toEqual(0x1A);
+          expect(cpu.registers.M).toEqual(2);
+          expect(cpu.registers.T).toEqual(8);
+          expect(mmu.read8).toHaveBeenCalledWith(0x201);
+        });
       });
     });
   });
 
   describe("LD r1, r2 - Load r2 into r1 - ", function() {
-    describe("LD A, A", function() {
-      it("loads A into A", function() {
-        cpu.registers.A = 0x33;
-        ops[0x7F]();
-        expect(cpu.registers.A).toEqual(cpu.registers.A);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
+    [
+      { dest: 'A', src: 'A', op: 0x7F },
+      { dest: 'A', src: 'B', op: 0x78 },
+      { dest: 'A', src: 'C', op: 0x79 },
+      { dest: 'A', src: 'D', op: 0x7A },
+      { dest: 'A', src: 'E', op: 0x7B },
+      { dest: 'A', src: 'H', op: 0x7C },
+      { dest: 'A', src: 'L', op: 0x7D },
+      { dest: 'B', src: 'A', op: 0x47 },
+      { dest: 'B', src: 'B', op: 0x40 },
+      { dest: 'B', src: 'C', op: 0x41 },
+      { dest: 'B', src: 'D', op: 0x42 },
+      { dest: 'B', src: 'E', op: 0x43 },
+      { dest: 'B', src: 'H', op: 0x44 },
+      { dest: 'B', src: 'L', op: 0x45 },
+      { dest: 'C', src: 'A', op: 0x4F },
+      { dest: 'C', src: 'B', op: 0x48 },
+      { dest: 'C', src: 'C', op: 0x49 },
+      { dest: 'C', src: 'D', op: 0x4A },
+      { dest: 'C', src: 'E', op: 0x4B },
+      { dest: 'C', src: 'H', op: 0x4C },
+      { dest: 'C', src: 'L', op: 0x4D },
+      { dest: 'D', src: 'A', op: 0x57 },
+      { dest: 'D', src: 'B', op: 0x50 },
+      { dest: 'D', src: 'C', op: 0x51 },
+      { dest: 'D', src: 'D', op: 0x52 },
+      { dest: 'D', src: 'E', op: 0x53 },
+      { dest: 'D', src: 'H', op: 0x54 },
+      { dest: 'D', src: 'L', op: 0x55 },
+      { dest: 'E', src: 'A', op: 0x5F },
+      { dest: 'E', src: 'B', op: 0x58 },
+      { dest: 'E', src: 'C', op: 0x59 },
+      { dest: 'E', src: 'D', op: 0x5A },
+      { dest: 'E', src: 'E', op: 0x5B },
+      { dest: 'E', src: 'H', op: 0x5C },
+      { dest: 'E', src: 'L', op: 0x5D },
+      { dest: 'H', src: 'A', op: 0x67 },
+      { dest: 'H', src: 'B', op: 0x60 },
+      { dest: 'H', src: 'C', op: 0x61 },
+      { dest: 'H', src: 'D', op: 0x62 },
+      { dest: 'H', src: 'E', op: 0x63 },
+      { dest: 'H', src: 'H', op: 0x64 },
+      { dest: 'H', src: 'L', op: 0x65 },
+      { dest: 'L', src: 'A', op: 0x6F },
+      { dest: 'L', src: 'B', op: 0x68 },
+      { dest: 'L', src: 'C', op: 0x69 },
+      { dest: 'L', src: 'D', op: 0x6A },
+      { dest: 'L', src: 'E', op: 0x6B },
+      { dest: 'L', src: 'H', op: 0x6C },
+      { dest: 'L', src: 'L', op: 0x6D }
+    ].forEach(function(i) {
+      describe("LD " + i.dest + "," + i.src, function() {
+        it("loads " + i.src + " into " + i.dest, function() {
+          cpu.registers[i.dest] = 0x33;
+          cpu.registers[i.src] = 0x22;
+          ops[i.op]();
+          expect(cpu.registers[i.dest]).toEqual(cpu.registers[i.src]);
+          expect(cpu.registers.M).toEqual(1);
+          expect(cpu.registers.T).toEqual(4);
+        });
       });
     });
+  });
 
-    describe("LD A, B", function() {
-      it("loads B into A", function() {
-        cpu.registers.A = 0x33;
-        cpu.registers.B = 0x11;
-        ops[0x78]();
-        expect(cpu.registers.A).toEqual(cpu.registers.B);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
-      });
-    });
-     
-    describe("LD A, C", function() {
-      it("loads C into A", function() {
-        cpu.registers.A = 0x33;
-        cpu.registers.C = 0x11;
-        ops[0x79]();
-        expect(cpu.registers.A).toEqual(cpu.registers.C);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
-      });
-    });
-
-    describe("LD A, D", function() {
-      it("loads D into A", function() {
-        cpu.registers.A = 0x33;
-        cpu.registers.D = 0x11;
-        ops[0x7A]();
-        expect(cpu.registers.A).toEqual(cpu.registers.D);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
-      });
-    });
-
-    describe("LD A, E", function() {
-      it("loads E into A", function() {
-        cpu.registers.A = 0x33;
-        cpu.registers.E = 0x11;
-        ops[0x7B]();
-        expect(cpu.registers.A).toEqual(cpu.registers.E);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
-      });
-    });
-
-    describe("LD A, H", function() {
-      it("loads H into A", function() {
-        cpu.registers.A = 0x33;
-        cpu.registers.H = 0x11;
-        ops[0x7C]();
-        expect(cpu.registers.A).toEqual(cpu.registers.H);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
-      });
-    });
-
-    describe("LD A, L", function() {
-      it("loads L into A", function() {
-        cpu.registers.A = 0x33;
-        cpu.registers.L = 0x11;
-        ops[0x7D]();
-        expect(cpu.registers.A).toEqual(cpu.registers.L);
-        expect(cpu.registers.M).toEqual(1);
-        expect(cpu.registers.T).toEqual(4);
-      });
-    });
-
-    describe("LD A, (HL)", function() {
-      it("loads value from address in HL into A", function() {
-        spyOn(mmu, 'read8').and.returnValue(0x30);
-        cpu.registers.A = 0x33;
-        cpu.registers.H = 0x31;
-        cpu.registers.L = 0xAB;
-        ops[0x7E]();
-        expect(cpu.registers.A).toEqual(0x30);
-        expect(cpu.registers.M).toEqual(2);
-        expect(cpu.registers.T).toEqual(8);
-        expect(mmu.read8).toHaveBeenCalledWith(0x31AB);
+  describe("LD r,(HL)", function() {
+    [
+      { dest: 'A', op: 0x7E },
+      { dest: 'B', op: 0x46 },
+      { dest: 'C', op: 0x4E },
+      { dest: 'D', op: 0x56 },
+      { dest: 'E', op: 0x5E },
+      { dest: 'H', op: 0x66 },
+      { dest: 'L', op: 0x6E }
+    ].forEach(function(i) {
+      describe("LD " + i.dest + ",(HL)", function() {
+        it("loads value from address in HL into " + i.dest, function() {
+          spyOn(mmu, 'read8').and.returnValue(0x30);
+          cpu.registers[i.dest] = 0x33;
+          cpu.registers.H = 0x31;
+          cpu.registers.L = 0xAB;
+          ops[i.op]();
+          expect(cpu.registers[i.dest]).toEqual(0x30);
+          expect(cpu.registers.M).toEqual(2);
+          expect(cpu.registers.T).toEqual(8);
+          expect(mmu.read8).toHaveBeenCalledWith(0x31AB);
+        });
       });
     });
   });
