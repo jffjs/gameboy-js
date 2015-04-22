2bf82c20bfa78222d17f8f9afbf8a2d2d2969829
jffjs <jffreyjs@gmail.com>
2015-04-22 17:01:09 -0400

    add jump opcodes

---
+++ test/opcodes/jump.js
@@ -0,0 +1,19 @@
+/*global require, describe, it, beforeEach*/
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var CPU = require('../../lib/cpu');
+var MMU = require('../../lib/mmu');
+var jump = require('../../lib/opcodes/jump');
+
+describe("Jump opcodes", function() {
+  var cpu, mockMMU, ops;
+
+  beforeEach(function() {
+    cpu = new CPU();
+    var mmu = new MMU();
+    mockMMU = sinon.mock(mmu);
+    ops = jump(cpu, mmu);
+    cpu.pc = 0x200;
+  });
+});
