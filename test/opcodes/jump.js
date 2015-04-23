608f42646248bd640ad47a39a2d75de012c03e08
Jeff Smith <jffreyjs@gmail.com>
2015-04-22 21:54:19 -0400

    JP nn

--- test/opcodes/jump.js
+++ test/opcodes/jump.js
@@ -16,4 +16,22 @@ describe("Jump opcodes", function() {
     ops = jump(cpu, mmu);
     cpu.pc = 0x200;
   });
+
+  describe("JP nn", function() {
+    it("jumps to 16-bit immediate value nn", function() {
+      mockMMU.expects('read16').once().withArgs(0x202).returns(0x34AB);
+      ops[0xC3]();
+      mockMMU.verify();
+      expect(cpu.pc).to.equal(0x34AB);
+    });
+
+    it("does not increment program counter", function() {
+      ops[0xC3]();
+      expect(cpu.incrementPC).to.be.false;
+    });
+
+    it("takes 3 machine cycles", function() {
+      expect(ops[0xC3]()).to.equal(3);
+    });
+  });
 });
