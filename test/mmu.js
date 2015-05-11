90e63252e9e696418a25007e8adb119c615b51ee
jffjs <jffreyjs@gmail.com>
2015-05-11 16:07:55 -0400

    implement read and write

--- test/mmu.js
+++ test/mmu.js
@@ -59,17 +59,94 @@ describe("MMU", function() {
     });
 
     it("reads from VRAM when address is between 8000h and A000h", function() {
-      mockGPU.expects('read').once().withArgs(0x50).returns(0x34);
+      mockGPU.expects('readVRAM').once().withArgs(0x50).returns(0x34);
       expect(mmu.read8(0x8050)).to.equal(0x34);
       mockGPU.verify();
     });
+
+    it("reads from switchable RAM bank when address is between A000h and C000h", function() {
+      mmu.eRAM[0x1100] = 0x34;
+      expect(mmu.read8(0xB100)).to.equal(0x34);
+    });
+
+    it("reads from internal RAM when address is between C000h and E000h", function() {
+      mmu.iRAM[0x1100] = 0x34;
+      expect(mmu.read8(0xD100)).to.equal(0x34);
+    });
+
+    it("reads from echo of internal RAM when address between E000h and FE00h", function() {
+      mmu.iRAM[0x100] = 0x34;
+      mmu.iRAM[0x1D00] = 0xAB;
+      expect(mmu.read8(0xFD00)).to.equal(0xAB);
+      expect(mmu.read8(0xE100)).to.equal(0x34);
+    });
+
+    it("reads from sprite attribute memory when address between FE00h and FEA0h", function() {
+      mockGPU.expects('readOAM').once().withArgs(0x90).returns(0xAB);
+      expect(mmu.read8(0xFE90)).to.equal(0xAB);
+    });
+
+    it("reads from zero-page memory when address greater or equal to FF80h", function() {
+      mmu.zRAM[0x40] = 0x12;
+      expect(mmu.read8(0xFFC0)).to.equal(0x12);
+    });
   });
 
-  describe.skip("read16", function() {
-    it("reads two-bytes at address, most significant byte is in address specified", function() {
-      mmu._memory[0x200] = 0xFF;
-      mmu._memory[0x201] = 0x89;
-      expect(mmu.read16(0x201)).to.equal(0x89FF);
+  describe("read16", function() {
+    it("reads two-bytes at address", function() {
+      mmu.ROM[0x200] = 0xFF;
+      mmu.ROM[0x201] = 0x89;
+      expect(mmu.read16(0x200)).to.equal(0x89FF);
+    });
+  });
+
+  describe("write8", function() {
+    it("cannot write to ROM", function() {
+      mmu.ROM[0x200] = 0xFF;
+      mmu.write8(0x200, 0x11);
+      expect(mmu.ROM[0x200]).to.equal(0xFF);
+    });
+
+    it("writes to VRAM when address is between 8000h and A000h", function() {
+      mockGPU.expects('writeVRAM').once().withArgs(0x50, 0x34);
+      mmu.write8(0x8050, 0x34);
+      mockGPU.verify();
+    });
+
+    it("writes to switchable RAM bank when address is between A000h and C000h", function() {
+      mmu.write8(0xA200, 0xAB);
+      expect(mmu.eRAM[0x200]).to.equal(0xAB);
+    });
+
+    it("writes to internal RAM when address is between C000h and E000h", function() {
+      mmu.write8(0xD100, 0x34);
+      expect(mmu.iRAM[0x1100]).to.equal(0x34);
+    });
+
+    it("writes to echo of internal RAM when address between E000h and FE00h", function() {
+      mmu.write8(0xFD00, 0xAB);
+      mmu.write8(0xE100, 0x34);
+      expect(mmu.iRAM[0x100]).to.equal(0x34);
+      expect(mmu.iRAM[0x1D00]).to.equal(0xAB);
+    });
+
+    it("writes to sprite attribute memory when address between FE00h and FEA0h", function() {
+      mockGPU.expects('writeOAM').once().withArgs(0x90, 0xAB);
+      mmu.write8(0xFE90, 0xAB);
+      mockGPU.verify();
+    });
+
+    it("writes to zero-page memory when address greater or equal to FF80h", function() {
+      mmu.write8(0xFFC0, 0x12);
+      expect(mmu.zRAM[0x40]).to.equal(0x12);
+    });
+  });
+
+  describe("write16", function() {
+    it("writes two-bytes at address", function() {
+      mmu.write16(0xA200, 0x89FF);
+      expect(mmu.eRAM[0x200]).to.equal(0xFF);
+      expect(mmu.eRAM[0x201]).to.equal(0x89);
     });
   });
 });
