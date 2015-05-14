738a745a183a7de58d2e51d8cbdc0625316b43a2
jffjs <jffreyjs@gmail.com>
2015-05-14 14:56:40 -0400

    gpu timings

---
+++ test/gpu.js
@@ -0,0 +1,17 @@
+/*global require, describe, it, beforeEach*/
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var GPU = require('../lib/gpu');
+
+describe("GPU", function() {
+  var gpu;
+
+  beforeEach(function() {
+    gpu = new GPU();
+  });
+
+  describe("execute", function() {
+
+  });
+});
