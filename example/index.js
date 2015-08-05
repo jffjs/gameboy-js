0521e5c6510ffbb6716ca87c023af7482a2136cd
Jeff Smith <jffreyjs@gmail.com>
2015-08-04 21:43:19 -0400

    filepicker

--- example/index.js
+++ example/index.js
@@ -5,29 +5,15 @@ var gb = new Gameboy();
 // render canvas
 var canvas = document.getElementById('lcd');
 var ctx = canvas.getContext('2d');
-ctx.putImageData(new ImageData(gb.gpu.screen, 160, 144), 0, 0);
+var render = function(screen) {
+  ctx.putImageData(new ImageData(screen, 160, 144), 0, 0);
+};
 
-
-var then = Date.now();
-
-function emulate(a) {
-  console.log("starting " + a);
+function emulate() {
   for(var i = 0; i < 60000; i++) {
-    gb.emulate(function() {
-      if (i % 30000 == 0) {
-        console.log(i);
-      }
-    });
+    gb.emulate(render);
   }
-  var now = Date.now();
-  console.log(now - then);
-  then = now;
-
-  console.log("ending " + a);
-
+  requestAnimationFrame(emulate);
 }
 
-requestAnimationFrame(function() { emulate(1); });
-requestAnimationFrame(function() { emulate(2); });
-requestAnimationFrame(function() { emulate(3); });
-requestAnimationFrame(function() { emulate(4); });
+requestAnimationFrame(emulate);
